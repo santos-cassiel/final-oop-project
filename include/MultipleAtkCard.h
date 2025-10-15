60a8b4049cff797e004d1e16d20b806420ab2f0f
Cassiel Santos <a1984813@adelaide.edu.au>
2025-10-15 15:26:15 +1030

    Added MultipleAtkCard header code

---
+++ include/MultipleAtkCard.h
@@ -0,0 +1,17 @@
+#ifndef MULTIPLEATKCARD_H
+#define MULTIPLEATKCARD_H
+
+#include "../include/AttackCard.h"
+
+class MultipleAtkCard : public AttackCard {
+ private:
+  int numTargets;
+
+ public:
+  MultipleAtkCard(std::string name, int power, std::string atkType,
+                  int numTargets);
+
+  void applyAttack(Tower targets[], int count);
+};
+
+#endif
