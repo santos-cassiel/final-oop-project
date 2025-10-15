70cf8c0ba230fca323b5e3561d58e2aec673bb6f
Cassiel Santos <a1984813@adelaide.edu.au>
2025-10-15 15:04:36 +1030

    Remade the Card class

--- include/Card.h
+++ include/Card.h
@@ -7,15 +7,21 @@ using namespace std;
 #include <string>
 
 class Card {
- private:
-  string name;
-  int damage;
-  string type;  // attack or heal card
+ protected:
+  std::string name;
+  int power;  // used power instead of damage name for variable to avoid
+              // redundancy
+  std::string type;
 
  public:
+  Card(std::string name, int power, std::string type);
+  virtual ~Card() = default;
+
+  std::string getName() const;
+  std::string getType() const;
+  int getPower() const;
+
   virtual void applyAttack(Tower& target) = 0;
-  int getDamage() const;
-  string getName() const;
 };
 
 #endif
