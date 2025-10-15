3bd979ae2b90448345817cd1b371d92887de02f0
Cassiel Santos <a1984813@adelaide.edu.au>
2025-10-15 15:30:39 +1030

    Added code for MultipleAtkCard methods

---
+++ src/MultipleAtkCard.cpp
@@ -0,0 +1,13 @@
+#include "../include/MultipleAtkCard.h"
+
+MultipleAtkCard::MultipleAtkCard(std::string name, int power,
+                                 std::string attackType, int numTargets)
+    : AttackCard(name, power, attackType) {
+  this->numTargets = numTargets;
+}
+
+void MultipleAtkCard::applyAttack(Tower targets[], int count) {
+  for (int i = 0; i < count && i < numTargets; i++) {
+    targets[i].takeDamage(power);
+  }
+}
