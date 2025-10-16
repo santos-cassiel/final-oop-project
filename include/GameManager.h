f67277d172de5ba629eeb9a347801340f2b98d13
Cassiel Santos <a1984813@adelaide.edu.au>
2025-10-17 01:39:03 +1030

    Formatted to google

--- include/GameManager.h
+++ include/GameManager.h
@@ -1,30 +1,32 @@
 #ifndef GAMEMANAGER_H
 #define GAMEMANAGER_H
 
-#include "../include/Player.h"
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <thread>
 
-class GameManager{
-    private:
-        Player player1;
-        Player player2;
-        Player* currentPlayer;
-        int timer;
-        bool gameOver;
+#include "../include/Player.h"
 
-    public:
-        GameManager(const std::string& name1, const std::string& name2, int gameTime = 180);
-        void startGame();
+class GameManager {
+ private:
+  Player player1;
+  Player player2;
+  Player* currentPlayer;
+  int timer;
+  bool gameOver;
 
-    public:
-        void switchTurn();
-        void checkGameStatus();
-        void updateTimer();
-        void endGame();
-        void playerTurn(Player& current, Player& opponent);
-        void displayStatus() const;
+ public:
+  GameManager(const std::string& name1, const std::string& name2,
+              int gameTime = 180);
+  void startGame();
+
+ public:
+  void switchTurn();
+  void checkGameStatus();
+  void updateTimer();
+  void endGame();
+  void playerTurn(Player& current, Player& opponent);
+  void displayStatus() const;
 };
 
 #endif
--- include/Player.h
+++ include/Player.h
@@ -2,27 +2,28 @@
 #define PLAYER_H
 
 #include <string>
-#include "../include/Tower.h"
+
 #include "../include/Deck.h"
+#include "../include/Tower.h"
 
-class Player{
-    private:
-        std::string name;
-        Tower towers[3];
-        Card* hand[3];
-        Deck deck;
+class Player {
+ private:
+  std::string name;
+  Tower towers[3];
+  Card* hand[3];
+  Deck deck;
 
-    public:
-        Player(std::string name);
+ public:
+  Player(std::string name);
 
-        const std::string& getName() const;
-        Tower* getTowers();
-        Card** getHand();
+  const std::string& getName() const;
+  Tower* getTowers();
+  Card** getHand();
 
-        void firstHandDraw();
-        void playCard(Card* chosen, Tower& target);
-        void drawCard(int index);
-        bool isDefeated() const;
+  void firstHandDraw();
+  void playCard(Card* chosen, Tower& target);
+  void drawCard(int index);
+  bool isDefeated() const;
 };
 
 #endif
