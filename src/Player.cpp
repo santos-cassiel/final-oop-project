7c9c415d595c4f1969b350b298ed01a3c886ab5a
Cassiel Santos <a1984813@adelaide.edu.au>
2025-10-17 01:39:25 +1030

    Formatted to google

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,51 +1,44 @@
 #include "../include/Player.h"
+
 #include <iostream>
 
-Player::Player(std::string name){
-    this->name = name;
+Player::Player(std::string name) { this->name = name; }
+
+void Player::firstHandDraw() {
+  for (int i = 0; i < 3; i++) {
+    hand[i] = deck.drawCard(hand, 3);
+  }
 }
 
-void Player::firstHandDraw(){
-    for (int i = 0; i < 3; i++){
-        hand[i] = deck.drawCard(hand, 3);
+void Player::playCard(Card* chosen, Tower& target) {
+  if (!chosen) return;
+
+  chosen->applyAttack(target);
+
+  for (int i = 0; i < 3; i++) {
+    if (hand[i] == chosen) {
+      hand[i] = deck.drawCard(hand, 3);
+      break;
     }
+  }
 }
 
-void Player::playCard(Card* chosen, Tower& target){
-    if (!chosen) return;
-
-    chosen->applyAttack(target);
-
-    for (int i = 0; i < 3; i++){
-        if (hand[i] == chosen){
-            hand[i] = deck.drawCard(hand, 3);
-            break;
-        }
-    }
+void Player::drawCard(int index) {
+  if (index >= 0 && index <= 3) {
+    hand[index] = deck.drawCard(hand, 3);
+  }
 }
 
-void Player::drawCard(int index){
-    if (index >=0 && index <= 3){
-        hand[index] = deck.drawCard(hand, 3);
-    }
+bool Player::isDefeated() const {
+  for (int i = 0; i < 3; i++) {
+    if (!towers[i].isDestroyed()) return false;
+  }
+
+  return true;
 }
 
-bool Player::isDefeated() const{
-    for (int i = 0; i < 3; i++){
-        if (!towers[i].isDestroyed()) return false;
-    }
+const std::string& Player::getName() const { return name; }
 
-    return true;
-}
+Tower* Player::getTowers() { return towers; }
 
-const std::string& Player::getName() const{
-    return name;
-}
-
-Tower* Player::getTowers(){
-    return towers;
-}
-
-Card** Player::getHand(){
-    return hand;
-}
+Card** Player::getHand() { return hand; }
